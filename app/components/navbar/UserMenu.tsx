1bb725f994f97beec9cda75f12d48d4f2a64fe94
Oubid <saideloubid2@gmail.com>
2023-07-06 01:02:54 +0100

    second commit

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -4,8 +4,10 @@ import { AiOutlineMenu } from 'react-icons/ai';
 import Avatar from '../Avatar';
 import { useCallback ,useState } from 'react';
 import MenuItem from './MenuItem';
+import useRegisterModal from '@/app/hooks/useRegisterModal'
 
 const UserMenu = () => {
+    const registerModal = useRegisterModal();
     const [isOpen, setIsOpen] = useState(false);
 
 const toggleOpen = useCallback(() => {
@@ -37,7 +39,7 @@ const toggleOpen = useCallback(() => {
                           />
                           <MenuItem  
                             label="Sign up"
-                            onClick={() => {}}              
+                            onClick={registerModal.onOpen}              
                           />
                         </>
                     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Nunito } from "next/font/google";
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Navbar from "./components/navbar/Navbar";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -23,6 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ToasterProvider />
+        <RegisterModal />
         <Navbar />
         {children}</body>
     </html>
